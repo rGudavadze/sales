67290f165dd33781b7b8bbafc9c86d64070620b8
roma_gudavadze <roma.gudavadze@gmail.com>
2023-08-08 17:08:29 +0400

    feat: add views for orders and margins

---
+++ src/apps/orders/views.py
@@ -0,0 +1,44 @@
+from rest_framework.generics import (
+    ListCreateAPIView,
+    RetrieveAPIView,
+    RetrieveUpdateAPIView,
+)
+
+from apps.orders.models import Margin, Order
+from apps.orders.serializers import MarginSerializer, OrderSerializer
+
+
+class OrderListAPIView(ListCreateAPIView):
+    """
+    APIView to create order or list all the orders.
+    """
+
+    serializer_class = OrderSerializer
+    queryset = Order.objects.all()
+
+
+class OrderDetailAPIView(RetrieveAPIView):
+    """
+    APIView to get specific order.
+    """
+
+    serializer_class = OrderSerializer
+    queryset = Order.objects.all()
+
+
+class MarginListAPIView(ListCreateAPIView):
+    """
+    APIView to create margin or list all the margins.
+    """
+
+    serializer_class = MarginSerializer
+    queryset = Margin.objects.all()
+
+
+class MarginDetailAPIView(RetrieveUpdateAPIView):
+    """
+    APIView to get or update specific margin.
+    """
+
+    serializer_class = MarginSerializer
+    queryset = Margin.objects.all()
