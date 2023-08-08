a9f824555020e7e86165b6da7ca9665c5ec64ba4
roma_gudavadze <roma.gudavadze@gmail.com>
2023-08-08 17:04:29 +0400

    feat: add __str__ method in Customer model

--- src/apps/customers/models.py
+++ src/apps/customers/models.py
@@ -6,3 +6,6 @@ from apps.base.models import BaseModel
 class Customer(BaseModel):
     company_id = models.CharField(max_length=128, help_text="Company ID")
     name = models.CharField(max_length=128, help_text="Name")
+
+    def __str__(self):
+        return f"{self.name} - {self.company_id}"
