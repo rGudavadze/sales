7cd8c343619ad3a680082530975bdca40188af51
roma_gudavadze <roma.gudavadze@gmail.com>
2023-08-13 18:49:10 +0400

    fix: resolve issue in orders app urls module

--- src/apps/orders/urls.py
+++ src/apps/orders/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 
-from apps.orders.views import OrderDetailAPIView, OrderListAPIView
+from apps.orders.views import (
+    MarginDetailAPIView,
+    MarginListAPIView,
+    OrderDetailAPIView,
+    OrderListAPIView,
+)
 
 urlpatterns = [
     path("", OrderListAPIView.as_view(), name="order-list"),
-    path("<uuid:pk>/", OrderListAPIView.as_view(), name="order-detail"),
-    path("margin/", OrderDetailAPIView.as_view(), name="margin-list"),
-    path("margin/<uuid:pk>/", OrderDetailAPIView.as_view(), name="margin-detail"),
+    path("<uuid:pk>/", OrderDetailAPIView.as_view(), name="order-detail"),
+    path("margin/", MarginListAPIView.as_view(), name="margin-list"),
+    path("margin/<uuid:pk>/", MarginDetailAPIView.as_view(), name="margin-detail"),
 ]
