d8c5aedb7cf4b4edede98d7238051292af18a77d
roma_gudavadze <roma.gudavadze@gmail.com>
2023-08-13 18:48:57 +0400

    fix: resolve issue in customers app urls module

--- src/apps/customers/urls.py
+++ src/apps/customers/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from apps.customers.views import CustomerListAPIView
+from apps.customers.views import CustomerListAPIView, CustomerRetrieveAPIView
 
 urlpatterns = [
     path("", CustomerListAPIView.as_view(), name="customer-list"),
-    path("<uuid:pk>/", CustomerListAPIView.as_view(), name="customer-detail"),
+    path("<uuid:pk>/", CustomerRetrieveAPIView.as_view(), name="customer-detail"),
 ]
