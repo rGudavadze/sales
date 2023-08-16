e5238f3d6a5fc007073ac23432ee523dd60cf5c3
roma_gudavadze <roma.gudavadze@gmail.com>
2023-08-16 23:00:41 +0400

    feat: add exceptions for invalid token

--- src/utils/exceptions.py
+++ src/utils/exceptions.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from rest_framework import status
 from rest_framework.exceptions import APIException
 
@@ -6,3 +7,11 @@ class InventoryAvailabilityException(APIException):
     status_code = status.HTTP_400_BAD_REQUEST
     default_detail = "The Inventory is not in sufficient quantity."
     default_code = "insufficient_error"
+
+
+class AuthenticationFailed(HttpResponse):
+    status_code = 401
+
+
+class NotAuthenticated(HttpResponse):
+    status_code = 401
