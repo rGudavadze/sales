546775d84647d8adb1b5711b59b8849a67aad719
roma_gudavadze <roma.gudavadze@gmail.com>
2023-08-16 16:05:41 +0400

    refactor: OrderListAPIView, override create method for custom logic implementation

--- src/apps/orders/views.py
+++ src/apps/orders/views.py
@@ -1,7 +1,8 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveAPIView
+from rest_framework.response import Response
 
 from apps.orders.models import Order
-from apps.orders.serializers import OrderSerializer
+from apps.orders.serializers import OrderCreateSerializer, OrderSerializer
 
 
 class OrderListAPIView(ListCreateAPIView):
@@ -9,8 +10,22 @@ class OrderListAPIView(ListCreateAPIView):
     APIView to create order or list all the orders.
     """
 
-    serializer_class = OrderSerializer
     queryset = Order.objects.all()
+
+    def get_serializer_class(self):
+        if self.request.method == "GET":
+            return OrderSerializer
+        return OrderCreateSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer_class()(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        # cost = serializer.context.get("cost")
+        # TODO: insert into accounting
+
+        return Response(serializer.data)
 
 
 class OrderDetailAPIView(RetrieveAPIView):
